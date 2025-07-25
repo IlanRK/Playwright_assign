66b4d94d2ab0aba4454f3355279fb0c0e9913d3e
IlanRK <karthigayani.mb@gmail.com>
2025-07-25 20:30:16 +0530

    Add files via upload
    week1

---
+++ numberType.js
@@ -0,0 +1,27 @@
+
+
+let number = 13;
+
+function numberType(number) {
+  switch (true) {
+    case number === 0:
+    console.log("Number is Neutral");
+      break;
+
+      case number < 0:
+      console.log("Number is Negative");
+      break;
+
+      case number > 0:
+      console.log("Number is Positive");
+      break;
+
+  
+    default:console.log(check);
+    
+    
+  }
+  
+}
+numberType(number)
+
---
+++ varletconst.js
@@ -0,0 +1,15 @@
+const browserName="Chrome";
+
+function getBrowserName() {
+    if (browserName === "Chrome") {
+        let browserName;  
+           
+    }
+    console.log(browserName);  
+
+  
+    
+    
+}
+
+getBrowserName(browserName);
